99565fad71f879c2a8f8ffe0a56c7116a3d6e8f7
Cesar Brazon <cesarbrazon10@gmail.com>
2023-08-22 19:16:37 +0200

    chore: expose bigint parse error
    * chore: expose bigint parse error
    
    * chore: bump version

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub use bigdecimal::BigDecimal as BigNumber;
 pub use serde_json as JSON;
 pub use std::collections::BTreeMap as Map;
 pub use serde_bytes as bytes;
-pub use num_bigint::BigInt;
+pub use num_bigint::{BigInt, ParseBigIntError};
 pub use wrappers::polywrap_bigint::BigIntWrapper;
 pub use wrappers::polywrap_json::JSONString;
 
