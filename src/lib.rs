630272846e059b999b6f783d29fce643218eba97
Cesar <cesarbrazon10@gmail.com>
2023-08-24 16:47:50 +0200

    chore: re-export serde_bytes with crates name

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub mod wrappers;
 pub use bigdecimal::BigDecimal as BigNumber;
 pub use serde_json as JSON;
 pub use std::collections::BTreeMap as Map;
-pub use serde_bytes as bytes;
+pub use serde_bytes;
 pub use num_bigint::{BigInt, ParseBigIntError};
 pub use wrappers::polywrap_bigint::BigIntWrapper;
 pub use wrappers::polywrap_json::JSONString;
